122847a04ce01b4a54b313583a6c6a404090a8b4
Bridger Tower <bridgertower@gmail.com>
2025-06-02 22:35:06 -0600

    refactor: update layout styles in RootLayout component for improved typography and readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased`}
+        className={`${fontSans.variable} ${fontMono.variable} font-sans font-light leading-tight antialiased`}
       >
         {children}
         <Analytics />
