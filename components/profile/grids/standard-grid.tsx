65bd7281a69d8f339634822c46d28ed39feab2db
Bridger Tower <bridgertower@gmail.com>
2025-06-19 12:04:42 -0600

    refactor: simplify StandardGrid component layout by consolidating image and title structure for improved readability

--- components/profile/grids/standard-grid.tsx
+++ components/profile/grids/standard-grid.tsx
@@ -21,21 +21,19 @@ export function StandardGrid({ projects, username }: StandardGridProps) {
         <Link
           key={project.project.id}
           href={`/${username}/${project.project.slug}`}
-          className="group relative p-6 hover:bg-accent/30 transition-colors border-r border-b border-border"
+          className="group p-4 hover:bg-accent/30 transition-colors border-r border-b border-border space-y-2"
         >
-          <div className="relative mb-3">
-            <AsyncImage
-              src={project.featuredImage!.url}
-              alt={project.project.title}
-              placeholder="shimmer"
-              width={project.featuredImage!.width}
-              height={project.featuredImage!.height}
-            />
-            <h3 className="text-sm font-medium absolute -bottom-6 line-clamp-1 left-0 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center gap-1 justify-between w-full">
-              {project.project.title}{" "}
-              <ArrowUpRight className="text-muted-foreground" size={16} />
-            </h3>
-          </div>
+          <AsyncImage
+            src={project.featuredImage!.url}
+            alt={project.project.title}
+            placeholder="shimmer"
+            width={project.featuredImage!.width}
+            height={project.featuredImage!.height}
+          />
+          <h3 className="text-sm line-clamp-1 leading-0 opacity-0 group-hover:opacity-100 transition-opacity flex items-center gap-2 justify-between">
+            {project.project.title}{" "}
+            <ArrowUpRight className="text-muted-foreground" size={16} />
+          </h3>
         </Link>
       ))}
     </div>
