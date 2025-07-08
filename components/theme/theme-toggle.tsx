8546a78174bf5134b08bcd262db9e536c99fc516
Bridger Tower <bridgertower@gmail.com>
2025-07-08 12:00:45 -0600

    wip

--- components/theme/theme-toggle.tsx
+++ components/theme/theme-toggle.tsx
@@ -2,28 +2,27 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import * as React from "react";
 
 export function ThemeToggle() {
-	const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
-	const toggleTheme = () => {
-		setTheme(theme === "light" ? "dark" : "light");
-	};
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
-	return (
-		<button
-			type="button"
-			className="h-9 w-9 relative flex items-center justify-center border"
-			onClick={toggleTheme}
-		>
-			{theme === "light" ? (
-				<Sun className="h-[1.2rem] w-[1.2rem]" />
-			) : (
-				<Moon className="h-[1.2rem] w-[1.2rem]" />
-			)}
+  return (
+    <button
+      type="button"
+      className="h-9 w-9 relative flex items-center justify-center border"
+      onClick={toggleTheme}
+    >
+      {theme === "light" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
 
-			<span className="sr-only">Toggle theme</span>
-		</button>
-	);
+      <span className="sr-only">Toggle theme</span>
+    </button>
+  );
 }
