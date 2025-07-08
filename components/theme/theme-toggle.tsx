678816550c18c28f3250797e88397ec6de620ebb
Bridger Tower <bridgertower@gmail.com>
2025-07-08 12:22:24 -0600

    fix: theme toggle hydration error

--- components/theme/theme-toggle.tsx
+++ components/theme/theme-toggle.tsx
@@ -2,6 +2,7 @@
 
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -11,18 +12,15 @@ export function ThemeToggle() {
   };
 
   return (
-    <button
+    <Button
       type="button"
       className="h-9 w-9 relative flex items-center justify-center border"
+      variant="outline"
       onClick={toggleTheme}
     >
-      {theme === "light" ? (
-        <Sun className="h-[1.2rem] w-[1.2rem]" />
-      ) : (
-        <Moon className="h-[1.2rem] w-[1.2rem]" />
-      )}
-
+      <Sun className="h-[1.2rem] w-[1.2rem] transition-opacity duration-200 dark:opacity-0 dark:absolute" />
+      <Moon className="h-[1.2rem] w-[1.2rem] transition-opacity duration-200 opacity-0 absolute dark:opacity-100 dark:relative" />
       <span className="sr-only">Toggle theme</span>
-    </button>
+    </Button>
   );
 }
