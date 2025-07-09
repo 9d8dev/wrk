b5e72a41852c6bb886efd7a6dd715eefc6385785
Bridger Tower <bridgertower@gmail.com>
2025-07-08 21:04:52 -0600

    refactor: streamline import structure by removing unused exports for improved clarity

--- components/admin/index.ts
+++ components/admin/index.ts
@@ -3,10 +3,7 @@
 export { AdminNav } from "./admin-nav";
 export { AdminSidebar } from "./admin-sidebar";
 export { DangerZone } from "./danger-zone";
-export {
-  ManageSubscriptionButton,
-  ProPlanCard,
-} from "./manage-subscription-button";
+export { ManageSubscriptionButton } from "./manage-subscription-button";
 // Subscription components
 export { SubscriptionSection } from "./subscription-section";
 export { UpgradeButton, UpgradePlanCard } from "./upgrade-button";
